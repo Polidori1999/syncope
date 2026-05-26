c5434e17ff2dbfabd2374bdde3a17e15f83bc865
polidori1999 <leo.polidori99@gmail.com>
2026-05-26 17:13:56 +0200

    test randoop

---
+++ generated-tests/randoop/AuthDataAccessor/randoop/AuthDataAccessorRandoopRegressionTest.java
@@ -0,0 +1,10 @@
+package org.apache.syncope.core.spring.security.randoop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AuthDataAccessorRandoopRegressionTest0.class, AuthDataAccessorRandoopRegressionTest1.class, AuthDataAccessorRandoopRegressionTest2.class, AuthDataAccessorRandoopRegressionTest3.class })
+public class AuthDataAccessorRandoopRegressionTest {
+}
+
